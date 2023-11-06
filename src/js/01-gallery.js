7f8b831be1b182e1fd42528f4ce927eb084d0d11
Andrey-Say <ansayapin83@gmail.com>
2023-11-05 23:10:11 -0500

    11

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,16 +1,13 @@
 import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
 const listImages = document.querySelector(".gallery");
-const listImgMarkap = createListImagesEl(galleryItems);
-listImages.insertAdjacentHTML("afterbegin", listImgMarkap);
 
 function createListImagesEl(images) {
   return images
     .map(({ description, original, preview }) => {
       return `<li class="gallery__item">
-          <a class="gallery__link" href="${original}" target="_self">
+          <a class="gallery__link" href="${original}"  target="_self">
             <img
               class="gallery__image"
               src="${preview}"
@@ -21,4 +18,20 @@ function createListImagesEl(images) {
         </li>`;
     })
     .join("");
+}
+
+const listImgMarkap = createListImagesEl(galleryItems);
+listImages.insertAdjacentHTML("afterbegin", listImgMarkap);
+
+listImages.addEventListener("click", onImageClick);
+
+function onImageClick(e) {
+  e.preventDefault();
+  if (e.target.nodeName !== "IMG") {
+    return;
+  }
+  const originalSizeImg = basicLightbox.create(
+    `<img src='${e.target.dataset.source}'>`
+  );
+  originalSizeImg.show();
 }
