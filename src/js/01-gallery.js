0caf2e26d6377a2e2f8c167b17261eefa2a34a6d
Andrey-Say <ansayapin83@gmail.com>
2023-11-05 19:16:18 -0500

    1

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,4 +1,24 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
 console.log(galleryItems);
+const listImages = document.querySelector(".gallery");
+const listImgMarkap = createListImagesEl(galleryItems);
+listImages.insertAdjacentHTML("afterbegin", listImgMarkap);
+
+function createListImagesEl(images) {
+  return images
+    .map(({ description, original, preview }) => {
+      return `<li class="gallery__item">
+          <a class="gallery__link" href="${original}" target="_self">
+            <img
+              class="gallery__image"
+              src="${preview}"
+              data-source="${original}"
+              alt="${description}"
+            />
+          </a>
+        </li>`;
+    })
+    .join("");
+}
