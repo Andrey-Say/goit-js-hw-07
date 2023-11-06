1b5851bee6ef216682d1686282be4c1a7c00d0d7
Andrey-Say <ansayapin83@gmail.com>
2023-11-05 23:49:34 -0500

    Create

--- src/js/02-lightbox.js
+++ src/js/02-lightbox.js
@@ -1,4 +1,29 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+const listImages = document.querySelector(".gallery");
+
+function createListImagesEl(images) {
+  return images
+    .map(({ description, original, preview }) => {
+      return `<li class="gallery__item">
+          <a class="gallery__link" href="${original}"  target="_self">
+            <img
+              class="gallery__image"
+              src="${preview}"
+              alt="${description}"
+            />
+          </a>
+        </li>`;
+    })
+    .join("");
+}
+
+const listImgMarkap = createListImagesEl(galleryItems);
+listImages.insertAdjacentHTML("afterbegin", listImgMarkap);
+
+const lightbox = new SimpleLightbox(".gallery a", {
+  captionType: "alt",
+  captionPosition: "bottom",
+  captionDelay: 250,
+});
